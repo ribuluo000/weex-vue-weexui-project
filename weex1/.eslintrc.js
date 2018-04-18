237f78390efba537310d5c8fdd332dd26e19f1fd
Nick <ribuluo000@163.com>
2018-04-18 17:57:36 +0800

    eslint

--- weex1/.eslintrc.js
+++ weex1/.eslintrc.js
@@ -1,35 +1,39 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true
-  },
-  globals: {
-    weex: true
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    'vue/no-parsing-error':  [2, {
-      "x-invalid-end-tag": false
-    }],
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    root : true,
+    parserOptions : {
+        parser : 'babel-eslint'
+    },
+    env : {
+        browser : true
+    },
+    globals : {
+        weex : true
+    },
+    extends : [
+        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+        'plugin:vue/essential',
+        // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+        'standard'
+    ],
+    // required to lint *.vue files
+    plugins : [
+        'vue'
+    ],
+    // add your custom rules here
+    rules : {
+        'vue/no-parsing-error' : [ 2, {
+            "x-invalid-end-tag" : false
+        } ],
+        // allow async-await
+        'generator-star-spacing' : 'off',
+        // allow debugger during development
+        'no-debugger' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        "semi" : [ 2, "always" ],//语句强制分号结尾
+        "semi-spacing" : [ 0, { "before" : false, "after" : true } ],//分号前后空格
+        "key-spacing": [0, { "beforeColon": true, "afterColon": true }],//对象字面量中冒号的前后空格
+        "indent": [2, 4],//缩进风格
+    }
 }
--- weex1/webpack.config.js
+++ weex1/webpack.config.js
@@ -1,28 +1,28 @@
 // You can see all the config in `./configs`.
-const  buildPlugins = require('./configs/plugin');
+const buildPlugins = require('./configs/plugin');
 let webpackConfig;
 module.exports = env => {
-  switch (env.NODE_ENV) {
+    switch (env.NODE_ENV) {
     case 'prod':
     case 'production':
-      webpackConfig = require('./configs/webpack.prod.conf');
-      break;
+        webpackConfig = require('./configs/webpack.prod.conf');
+        break;
     case 'test':
     case 'testing':
-      webpackConfig = require('./configs/webpack.test.conf');
-      break;
+        webpackConfig = require('./configs/webpack.test.conf');
+        break;
     case 'plugin':
-      buildPlugins();
+        buildPlugins();
     case 'common':
-      webpackConfig = require('./configs/webpack.common.conf');
-      break;
+        webpackConfig = require('./configs/webpack.common.conf');
+        break;
     case 'release':
-      webpackConfig = require('./configs/webpack.release.conf');
-      break;
+        webpackConfig = require('./configs/webpack.release.conf');
+        break;
     case 'dev':
     case 'development':
     default:
-      webpackConfig = require('./configs/webpack.dev.conf');
-  }
-  return webpackConfig;
-}
+        webpackConfig = require('./configs/webpack.dev.conf');
+    }
+    return webpackConfig;
+};
