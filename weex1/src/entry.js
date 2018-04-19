44214a960e851e63f53855561afe8e38ea049c85
Nick <ribuluo000@163.com>
2018-04-19 17:12:54 +0800

    weex-ui demo

--- weex1/src/entry.js
+++ weex1/src/entry.js
@@ -2,7 +2,7 @@
 
 /* weex initialized here, please do not move this line */
 const router = require('./router');
-const App = require('@/weexui/weexui.vue');
+const App = require('@/weexui/example/button/index.vue');
 /* eslint-disable no-new */
 new Vue(Vue.util.extend({ el : '#root', router }, App));
 router.push('/');
