5a7afb4c0196c4289a8f9bf2640aa6aae39889c8
Nick <ribuluo000@163.com>
2018-04-19 19:30:12 +0800

    Revert "eslint"
    This reverts commit 237f78390efba537310d5c8fdd332dd26e19f1fd.

--- weex1/configs/helper.js
+++ weex1/configs/helper.js
@@ -3,18 +3,18 @@ const path = require('path');
 const ROOT = path.resolve(__dirname, '..');
 
 const root = (args) => {
-    return path.join(ROOT, 'src', args);
-};
+  return path.join(ROOT, 'src', args);
+}
 const rootNode = (args) => {
-    return path.join(ROOT, args);
-};
+  return path.join(ROOT, args);
+}
 
 const resolve = (dir) => {
-    return path.join(__dirname, '..', dir);
-};
+  return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
-    root,
-    rootNode,
-    resolve
-};
+  root,
+  rootNode,
+  resolve
+}
--- weex1/src/entry.js
+++ weex1/src/entry.js
@@ -1,8 +1,9 @@
-/* global Vue */
+/*global Vue*/
 
 /* weex initialized here, please do not move this line */
 const router = require('./router');
 const App = require('@/index.vue');
 /* eslint-disable no-new */
-new Vue(Vue.util.extend({ el : '#root', router }, App));
+new Vue(Vue.util.extend({el: '#root', router}, App));
 router.push('/');
+
